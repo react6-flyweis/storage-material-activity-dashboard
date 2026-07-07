aaccdbd9b583d8e9d329ce0005a7c7115b391799
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-07-07 18:03:26 +0530

    chore: update main application entry or setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
+// import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { ErrorBoundary } from "@/components/common/ErrorBoundary.tsx"
 import App from "./App.tsx"
 import "./index.css"
@@ -20,9 +20,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider defaultTheme="system" storageKey="theme">
+        {/* <ThemeProvider defaultTheme="system" storageKey="theme"> */}
           <App />
-        </ThemeProvider>
+        {/* </ThemeProvider> */}
       </QueryClientProvider>
     </ErrorBoundary>
   </StrictMode>
